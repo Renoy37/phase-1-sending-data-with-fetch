1fb40c6d0f1f81ed2f2938a674f802d1d31394ac
Robins <robinmgk077@gmail.com>
2024-01-15 14:34:13 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+// // Add your code here
+
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      document.body.innerHTML += data.id;
+      return data;
+    })
+    .catch((error) => {
+      document.body.innerHTML += error.message;
+    });
+}
+submitData("Steve", "steve@steve.com");
